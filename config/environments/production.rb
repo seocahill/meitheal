6ee8966e6c18065402fa65cf6c55dca11a39c6ef
Seosamh Ó Cathail <seosamh@seocahill.com>
2026-03-11 00:04:45 +0100

    Update domain references in configuration files
    - Changed the host from "beta.thencf.art" to "thencf.art" in both deploy configuration and production environment settings for consistency.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "beta.thencf.art" }
+  config.action_mailer.default_url_options = { host: "thencf.art" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   config.action_mailer.delivery_method = :smtp
