22a0e306316f3dc81bf7bece4c268938b732ddd2
Seosamh Ó Cathail <seosamh@seocahill.com>
2026-04-09 23:52:19 +0200

    Update actiontext CSS for improved image handling and adjust Sentry configuration to enable in production environments.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,4 +4,5 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [ :active_support_logger ]
   config.dsn = Rails.application.credentials.dig(:sentry_dsn)
   config.traces_sample_rate = 0.3
+  config.enabled_environments = %w[production]
 end
